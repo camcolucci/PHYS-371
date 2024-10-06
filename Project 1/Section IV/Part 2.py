6205731c7fce0eb9e18175f6461240e7f8f36abb
Cam Colucci <ccolucci@sandiego.edu>
2024-10-05 22:07:05 -0700

    fixed the beta term

--- Project 1/Section IV/Part 2.py
+++ Project 1/Section IV/Part 2.py
@@ -5,6 +5,8 @@ the right side of the damped oscillator equation
 """
 def linear_oscillator(omega_0, omega_d, f_0):
     dxdt = v                                                            # Defines the first derivative as velocity
-    dvdt = -2 * beta * v - omega_0**2 * x + f_0 * np.sin(omega_d * t)   # Define the equation of the damped oscillator
+    
+    dvdt = -2 * gamma * omega_0 * v - omega_0**2 * x + f_0 * np.sin(omega_d * t)    # Define the equation of the damped oscillator
+                                                                                    # where gamma is some damping constant
     return dxdt, dvdt                                                   # Returns values for the velocity and the 
                                                                             # acceleration of the oscillator
